1e47918a79c01e53444345a3e969acb415906033
RohanGoswami <120251869+Rohan-1103@users.noreply.github.com>
2026-01-02 16:01:37 +0530

    Update app title and add header and subheader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Codeit Internship Demo App)
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+st.header("Heading")
+st.subheader("Sub heading")
